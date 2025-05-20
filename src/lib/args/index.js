e99add450cdc932122e3b2204fd908b57a62f817
Mohammed Hamdoune <sidlynx@gmail.com>
2025-05-20 21:00:04 +0200

    add central index file to make the import from args lib folder easy

---
+++ src/lib/args/index.js
@@ -0,0 +1,6 @@
+export * from './get-argument.js';
+export * from './get-arguments.js';
+export * from './get-filter.js';
+export * from './is-argument-count.js';
+export * from './is-argument-filter.js';
+export * from './is-one-argument.js';
