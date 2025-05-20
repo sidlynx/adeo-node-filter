d8cc3d5ef0f6a69def555106e6c30427bfb0c459
Mohammed Hamdoune <sidlynx@gmail.com>
2025-05-20 21:11:44 +0200

    update the entry point with the desired functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-console.log("Welcome from index.js");
+import { countries } from '../data.js';
+
+import { 
+    getCountriesWithItemsCount,
+    getFilterCountriesByAnimals,
+} from './lib/data/index.js';
+
+ import { 
+    isOneArgument,
+    isArgumentFilter,
+    isArgumentCount,
+    getArgument,
+    getFilter,
+ } from './lib/args/index.js';
+
+import { log } from './lib/log.js';
+
+let result;
+let success = false;
+
+
+if(isOneArgument()){
+  const arg = getArgument();
+  if(isArgumentFilter(arg)){
+    const filter = getFilter(arg);
+    result = getFilterCountriesByAnimals(countries,filter);
+    success = true;
+  }else if(isArgumentCount(arg)){
+    result = getCountriesWithItemsCount(countries);
+    success = true;
+  }
+}
+
+if(success){
+    log(result);
+}
+else{
+  console.log("Please provide a valid argument");
+  console.log("For filtering by animals, use: node index.js --filter=your_filter");
+  console.log("For adding counts, use: node index.js --count");
+}
